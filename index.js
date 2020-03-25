27c0dbb84e7d25deec837f1695c5a56007c3a80f
Guillermo Alfaro <galfarotolon@gmail.com>
2020-03-25 15:35:24 -0500

    Finished first exercise.

--- index.js
+++ index.js
@@ -39,8 +39,26 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
+function Person(name, age) {
 
+  this.name = name,
+    this.age = age,
+    this.stomach = [];
+
+}
+
+Person.prototype.eat = function (someFood) {
+  if (someFood <= 10) {
+    this.stomach.push(someFood);
+  }
+};
+
+Person.prototype.poop = function () {
+  this.stomach = [];
+};
+
+Person.prototype.toString = function () {
+  return `${this.name},${this.age}`;
 }
 
 /*
@@ -88,8 +106,16 @@ function Baby() {
 ///////// END OF CHALLENGE /////////
 if (typeof exports !== 'undefined') {
   module.exports = module.exports || {}
-  if (Airplane) { module.exports.Airplane = Airplane }
-  if (Person) { module.exports.Person = Person }
-  if (Car) { module.exports.Car = Car }
-  if (Baby) { module.exports.Baby = Baby }
+  if (Airplane) {
+    module.exports.Airplane = Airplane
+  }
+  if (Person) {
+    module.exports.Person = Person
+  }
+  if (Car) {
+    module.exports.Car = Car
+  }
+  if (Baby) {
+    module.exports.Baby = Baby
+  }
 }
