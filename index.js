3babc6c3bc4a633c6aea140ce6a3c5fb894f8dd2
Guillermo Alfaro <galfarotolon@gmail.com>
2020-03-25 17:31:45 -0500

    finished main exercises and answered the 4 questions.

--- index.js
+++ index.js
@@ -75,9 +75,20 @@ Person.prototype.toString = function () {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
+function Car(model, milesPerGallon) {
+  this.model = model,
+    this.milesPerGallon = milesPerGallon,
+    this.tank = 0,
+    this.odometer = 0
+}
+
+Car.prototype.fill = function (gallons) {
+
+  this.tank = this.tank + gallons;
 
 }
+
+
 
 /*
   TASK 3
@@ -86,18 +97,40 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
+function Baby(name, age, favoriteToy) {
+  this.name = name;
+  this.age = age;
+  this.favoriteToy = favoriteToy;
 
 }
+Baby.prototype = Object.create(Person.prototype);
+
+Baby.prototype.play = function () {
+  return `Playing with ${this.favoriteToy}`;
+}
+
 
 /* 
   TASK 4
 
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+
+  1. Window/Global Object Binding - When the keyword "this" is not set or bound within any context or scope (i.e. a function), it will
+  automatically refer to the window, which is the whole Javascript language object. 
+
+  2. Implicit Binding - When you call a function and when the function is invoked, whatever is to the left of the dot
+   is what the "this" keyword is going to reference. This is a function invocation. 
+
+  3. New binding - New binding occurs when creating a Constructor Function. In this case, the keyword "this" refers to the
+  specific instance of the object that is created and returned by the constructor function. 
+
+  4. Explicit binding - When .call or .apply methods are used, this is an example of explicit binding. 
+  Constructor functions can be modified or altered by using explicit binding. 
+
+ 
+
+
+
 */
 
 
